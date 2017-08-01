b1e817ff991302271ba13135a817a54343335d03
dave <david.sehnal@gmail.com>
2017-08-01 20:11:28 +0200

    types

--- src/mini-lisp/type.ts
+++ src/mini-lisp/type.ts
@@ -2,9 +2,9 @@
  * Copyright (c) 2017 David Sehnal, licensed under MIT, See LICENSE file for more info.
  */
 
-interface Type<T = any> { kind: 'value', name: string, parent?: Type, '@type': T }
+interface Type<T = any> { name: string, parent?: Type, '@type': T }
 
-function Type<T>(name: string, parent?: Type): Type<T> { return { kind: 'value', name, parent, '@type': 0 as any } }
+function Type<T>(name: string, parent?: Type): Type<T> { return { name, parent, '@type': 0 as any } }
 
 namespace Type {
     export const AnyValue = Type<any>('value');
