caf1c89bc19bc8478520677d1f2701d595aff269
dave <david.sehnal@gmail.com>
2017-08-01 21:23:35 +0200

    x

--- src/mol-ql/symbols/structure.ts
+++ src/mol-ql/symbols/structure.ts
@@ -18,15 +18,48 @@ const type = {
     '@header': 'Types',
     elementSymbol: symbol(Arguments.Dictionary({ 0: Argument(Type.Str) }), Types.ElementSymbol),
     authResidueId: symbol(Arguments.Dictionary({
-        0: Argument(Type.Str, { description: 'auth_asym_id' }),
-        1: Argument(Type.Num, { description: 'auth_seq_id' }),
-        2: Argument(Type.Str, { description: 'pdbx_PDB_ins_code' })
-    }), Types.ElementSymbol),
+            0: Argument(Type.Str, { description: 'auth_asym_id' }),
+            1: Argument(Type.Num, { description: 'auth_seq_id' }),
+            2: Argument(Type.Str, { description: 'pdbx_PDB_ins_code' })
+        }), Types.ElementSymbol),
     labelResidueId: symbol(Arguments.Dictionary({
-        0: Argument(Type.Str, { description: 'label_entity_id' }),
-        1: Argument(Type.Str, { description: 'label_asym_id' }),
-        2: Argument(Type.Num, { description: 'label_auth_seq_id' }),
-        3: Argument(Type.Str, { description: 'pdbx_PDB_ins_code' })
-    }), Types.ElementSymbol),
+            0: Argument(Type.Str, { description: 'label_entity_id' }),
+            1: Argument(Type.Str, { description: 'label_asym_id' }),
+            2: Argument(Type.Num, { description: 'label_auth_seq_id' }),
+            3: Argument(Type.Str, { description: 'pdbx_PDB_ins_code' })
+        }), Types.ElementSymbol),
 };
 
+const atomProperty ={
+
+}
+
+const generator = {
+
+}
+
+const modifier = {
+
+}
+
+const filter = {
+
+}
+
+const combinator = {
+
+}
+
+const atomSet = {
+
+}
+
+export default {
+    type,
+    atomProperty,
+    generator,
+    modifier,
+    filter,
+    combinator,
+    atomSet
+}
